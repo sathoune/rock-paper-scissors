2348c678e11eebca690070bc4633b21514eced3d
m <michal5bargiel@gmail.com>
2019-09-27 00:43:31 +0200

    View of scoreboard and fighters

--- spec/test.js
+++ spec/test.js
@@ -1,13 +1,17 @@
-const {Builder, By} = require('selenium-webdriver');
-const chrome = require('chromedriver');
+const {Builder, By} = require('selenium-webdriver')
+const chrome = require('chromedriver')
 
 describe('website', () => {
-  it('has #scissors element', async () => {
+  it('has #scissors, #rock and #paper elements', async () => {
     const driver = await new Builder().forBrowser('chrome').build()
     try {
       await driver.get('http://127.0.0.1:3000/')
-      const result = await driver.findElement(By.id('scissors'))
-      expect(await result.getAttribute('innerHTML')).toEqual('hi')
+      const scissorsElement = await driver.findElement(By.id('scissors'))
+      const rockElement     = await driver.findElement(By.id('rock'))
+      const paperElement    = await driver.findElement(By.id('paper'))
+      expect(await scissorsElement.getAttribute('innerHTML')).toEqual('scissors')
+      expect(await rockElement    .getAttribute('innerHTML')).toEqual('rock')
+      expect(await paperElement   .getAttribute('innerHTML')).toEqual('paper')
     } finally {
       await driver.quit()
     }
--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 const http  = require('http')
 const fs    = require('fs')
+const path  = require('path')
 const port  = 3000
 const adress = '127.0.0.1'
 
 http.createServer( async (req, res) => {
-    const content = await fs.readFileSync('./index.html')
+    const indexPage = await fs.readFileSync(path.join(__dirname, 'public/index.html'))
     res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end(content, 'utf-8')
+    res.end(indexPage, 'utf-8')
 }).listen(port, adress, () => {
   console.log(`Server running at http://${adress}:${port}/`)
 })
