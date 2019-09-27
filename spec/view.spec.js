ceef948eb0f30058a510fe0079ae2d425ddca61c
m <michal5bargiel@gmail.com>
2019-09-27 04:24:44 +0200

    User can fight against rock-lover

--- spec/view.spec.js
+++ spec/view.spec.js
@@ -4,19 +4,17 @@ const chrome = require('chromedriver')
 const baseUrl = 'http://127.0.0.1:3000/'
 
 describe('website', () => {
-  it('has #scissors, #rock, #paper and #score elements', async () => {
-    const driver = await new Builder().forBrowser('chrome').build()
+  it('has #scissors, #rock, #paper elements', async () => {
+    const driver = new Builder().forBrowser('chrome').build()
     try {
       await driver.get(baseUrl)
       const scissorsElement = await driver.findElement(By.id('scissors'))
       const rockElement     = await driver.findElement(By.id('rock'))
       const paperElement    = await driver.findElement(By.id('paper'))
-      const scoreElement    = await driver.findElement(By.id('score'))
-      expect(await scissorsElement.getAttribute('innerHTML')).toEqual('scissors')
+    expect(await scissorsElement.getAttribute('innerHTML')).toEqual('scissors')
       expect(await rockElement    .getAttribute('innerHTML')).toEqual('rock')
       expect(await paperElement   .getAttribute('innerHTML')).toEqual('paper')
-      expect(await scoreElement)  .not.toBe(undefined)
-    } finally {
+   } finally {
       await driver.quit()
     }
   })
@@ -24,10 +22,12 @@ describe('website', () => {
 
 describe('Tony opens page wanting to play R-P-S', () => {
   it('he chooses scissors', async () => {
-    const driver = await new Builder().forBrowser('chrome').build()
+    const driver = new Builder().forBrowser('chrome').build()
     try {
       await driver.get(baseUrl)
-      console.log(await driver.findElement(By.id('scissors')).click())
+      await driver.findElement(By.id('scissors')).click()
+      const outcomeText = await driver.findElement(By.id('outcome')).getAttribute('innerText')
+      expect(outcomeText.length > 0).toBe(true)
     } finally {
       await driver.quit()
     }
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,1 +1,52 @@
-console.log('hi')
+const ajax = (
+    method, 
+    endpoint, 
+    callback= xhr => console.log(xhr.status), 
+    errorCallback = (xhr) => console.log(xhr.status)
+  ) => {
+  const xhr = new XMLHttpRequest()
+  xhr.open(method, endpoint)
+  xhr.send()
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4) {
+      if(xhr.status === 200) callback(xhr)
+      else errorCallback(xhr) 
+    }  
+  }
+}
+
+const updateScore = outcome => {
+  if(outcome === 'win') {
+    const playerScoreElement = document.getElementById('player-score')
+    playerScoreElement.innerText = Number(playerScoreElement.innerText) + 1
+  } else if (outcome === 'lose') {
+    const opponentScoreElement = document.getElementById('opponent-score')
+    opponentScoreElement.innerText = Number(opponentScoreElement.innerText) + 1
+  }
+}
+
+const updateMessage = (outcome, weapon0, weapon1) => {
+  document.getElementById('outcome').innerText = `Player ${outcome}s with ${weapon0} against ${weapon1}`
+}
+
+const requestOpponent = playerWeapon => ajax(
+  'GET', 
+  'random', 
+  xhr => {
+    const data = JSON.parse(xhr.responseText)
+    const outcome = fight(rules)(playerWeapon, data.opponent)
+    updateScore(outcome)
+    updateMessage(outcome, playerWeapon, data.opponent)
+  }
+) 
+
+const fight = rules => (p1, p2) => {
+  return (p1 === p2) ? 'draw' : (rules[p1].includes(p2) ? 'win' : 'lose') 
+
+}
+
+const rules = {
+  rock:     ['scissors'],
+  paper:    ['rock'],
+  scissors: ['paper']
+}
