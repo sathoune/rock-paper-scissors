f551fad4da83ca644e5002f14aa4051a580bc59c
m <michal5bargiel@gmail.com>
2019-09-28 21:29:11 +0200

    Catched syntaxerror for json parsing; Created tests for wrong fight submissions

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -2,12 +2,15 @@ const Axios = require("axios")
 const {weapons} = require('../src/fight')
 const baseUrl = 'http://127.0.0.1:3000/'
 
+const POST = 'POST'
+const fightUrl = baseUrl + 'fight'
+
 describe('/fight', () => {
-  it('returns fighter weapons and the result', async () => {
+  it('Given player\'s weapon responds with fight results', async () => {
     const weapon = 'rock'
     const res = await Axios({
-      url: baseUrl + 'fight', 
-      method: 'POST',
+      url: fightUrl, 
+      method: POST,
       headers: {
         'Content-Type': 'application/json',
         data: JSON.stringify({
@@ -20,13 +23,75 @@ describe('/fight', () => {
     expect(weapons.includes(fight.opponentWeapon)).toBe(true)
     expect(fight.result).not.toBe(undefined)
   })
+
+  it('allows only "application/json" content', async () => {
+    const weapon = 'paper'
+    await Axios({
+      url: fightUrl,
+      method: POST,
+      headers: {
+        'Content-Type': 'text/html',
+        data: weapon
+      }
+    }).catch( ({response}) => {
+      expect(response.status).toBe(400)
+      expect(response.data).toBe('Wrong type of content')
+    })
+  })
+
+  it('requires proper object format', async () => {
+    await Axios({
+      url: fightUrl,
+      method: POST,
+      headers: {
+        'Content-Type': 'application/json',
+        data: ''
+      }
+    }).catch( ({response}) => {
+      expect(response.status).toBe(400)
+      expect(response.data).toBe('File battle request properly!')
+    })
+  })
+
+  it('requires player to send their weapon of choice', async () => {
+    await Axios({
+      url: fightUrl,
+      method: POST,
+      headers: {
+        'Content-Type': 'application/json',
+        data: JSON.stringify({
+          
+        })
+      }
+    }).catch( ({response}) => {
+      expect(response.status).toBe(400)
+      expect(response.data).toBe('Do not come unarmed!')
+    }) 
+  })
+
+  it('requires player to file a legal weapon', async () => {
+    const wrongWeapon = 'morgernstern'
+    await Axios({
+      url: fightUrl,
+      method: POST,
+      headers: {
+        'Content-Type': 'application/json',
+        data: JSON.stringify({
+          weapon: wrongWeapon
+        })
+      }
+    }).catch( ({response}) => {
+      expect(response.status).toBe(400)
+      expect(response.data).toBe('Illegal weapon!')
+    })
+  })
 })
 
 describe('/', () => {
   it('renders a base page', async () => {
     const res = await Axios.get(baseUrl)
     expect(res.headers['content-type']).toBe('text/html')
-    expect(res.data.includes('!DOCTYPE')).toBe(true)
+    expect(res.data.includes('!DOCTYPE html')).toBe(true)
   })
 })
 
--- src/serverFunctions.js
+++ src/serverFunctions.js
@@ -5,17 +5,28 @@ const sanitizeFight = (req, res) => {
     badRequest(res, 'Wrong type of content')
     return false
   }
-  const {weapon: playerWeapon} = JSON.parse(req.body)
+  const {weapon: playerWeapon, error} = parseJSON(req.body)
+  if(error === 'SyntaxError') {
+    badRequest(res, 'File battle request properly!')
+  }
   if(!playerWeapon){
-    badRequest(res, 'Do not come unarmed')
+    badRequest(res, 'Do not come unarmed!')
     return false
   }
   if(!weapons.includes(playerWeapon)){
-    badRequest(res, 'Illegal weapon')
+    badRequest(res, 'Illegal weapon!')
     return false
   }
   return true
 } 
+
+const parseJSON = json => {
+  try {
+    return JSON.parse(json)
+  } catch (e) {
+    return {error: e.name}
+  }
+}
 
 const badRequest = (res, msg) => {
   res.writeHead(400)
