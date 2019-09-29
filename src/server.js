8bb5856ea13c55ad8ceb5fd38e44aaa74c10141c
m <michal5bargiel@gmail.com>
2019-09-29 17:56:47 +0200

    Revert "Created readme with instructions; changed code for deploy"
    This reverts commit 5463fe164b6ebb0f7bf45af31ceff70bbd7c412c.

--- src/server.js
+++ src/server.js
@@ -2,7 +2,10 @@ const http      = require('http')
 
 const router    = require('./router')
 const PORT      = process.env.PORT || 3000
+const ADDRESS    = process.env.ADDRESS || '127.0.0.1'
 
-const server = http.createServer(router).listen(PORT)
+const server = http.createServer(router).listen(PORT, ADDRESS, () => {
+  console.log(`Server running at http://${ADDRESS}:${PORT}/`)
+})
 
 module.exports = server
