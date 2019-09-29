5463fe164b6ebb0f7bf45af31ceff70bbd7c412c
m <michal5bargiel@gmail.com>
2019-09-29 17:22:53 +0200

    Created readme with instructions; changed code for deploy

--- src/server.js
+++ src/server.js
@@ -2,10 +2,7 @@ const http      = require('http')
 
 const router    = require('./router')
 const PORT      = process.env.PORT || 3000
-const ADDRESS    = process.env.ADDRESS || '127.0.0.1'
 
-const server = http.createServer(router).listen(PORT, ADDRESS, () => {
-  console.log(`Server running at http://${ADDRESS}:${PORT}/`)
-})
+const server = http.createServer(router).listen(PORT)
 
 module.exports = server
